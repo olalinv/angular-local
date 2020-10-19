26f461b1ff2a6124043a330adcfc4c034109c269
Óscar Lalín Valcárcel <olalin@atsistemas.com>
2020-10-19 10:31:00 +0200

    Changed app name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'pser-local'`, () => {
+  it(`should have as title 'angular-local'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pser-local');
+    expect(app.title).toEqual('angular-local');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('pser-local app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-local app is running!');
   });
 });
